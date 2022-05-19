4d054b6ac7e8d88d7138913bc9014cf7b257a470
zen-machina <rojosudo@gmail.com>
2022-05-19 13:18:32 -0700

    broke down component <Gallery> from work page

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,68 @@
+import { Flex, Grid, Image, useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import ImgModal from "../components/ImgModal";
+import getImageUrl from "../tools/getImageUrl";
+
+function Gallery() {
+    const imgURLs = Object.keys(getImageUrl());
+    // chakra ui hook, using for modal
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    // state for image src on handleClick
+    const [currentImgSrc, setCurrentImgSrc] = useState("");
+
+    // sets image source state on click for modal popup
+    const handleModalClick = (e) => {
+        const imgSrcRaw = e.target.src;
+        const imgSRC = imgSrcRaw.replace("http://localhost:3000", "");
+        setCurrentImgSrc(imgSRC);
+        onOpen();
+    };
+  return (
+        <Flex mx="6" justify="center">
+            <Grid
+                templateColumns={["1fr", "1fr ", "1fr 1fr", "1fr 1fr 1fr"]}
+                mt="50px"
+                maxW={1800}
+                gap={6}
+            >
+                {/* maps through image urls ( imgURLs ) */}
+                {imgURLs.map((img, index) => {
+                    return (
+                        <Flex
+                            key={index}
+                            minW="245px"
+                            maxH="400px"
+                            _hover={{
+                                backgroundColor: "black",
+                                borderRadius: "xl",
+                            }}
+                        >
+                            <Image
+                                src={`${img}`}
+                                w="100%"
+                                fit="cover"
+                                borderRadius="xl"
+                                _hover={{
+                                    opacity: "0.4",
+                                }}
+                                onClick={handleModalClick}
+                            />
+                        </Flex>
+                    );
+                })}
+
+                {/* image modal for <Image> onClic */}
+                <ImgModal
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    src={currentImgSrc}
+                ></ImgModal>
+            </Grid>
+        </Flex>
+    );
+}
+
+  )
+}
+
+export default Gallery
--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -1,62 +1,7 @@
-import { Flex, Grid, Image, useDisclosure } from "@chakra-ui/react";
-import { useState } from "react";
-import ImgModal from "../components/ImgModal";
-import getImageUrl from "../tools/getImageUrl";
+import Gallery from "../components/Gallery";
 
-function Work(props) {
-    const imgURLs = Object.keys(getImageUrl());
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [currentImgSrc, setCurrentImgSrc] = useState("");
-
-    // need a way to pass the current image source on the onClick event to my custom ImgModal
-    const handleClick = (e) => {
-        const imgSrcRaw = e.target.src;
-        const imgSRC = imgSrcRaw.replace("http://localhost:3000", "");
-        setCurrentImgSrc(imgSRC);
-        console.log(currentImgSrc);
-        onOpen();
-    };
-
-    return (
-        <Flex mx="6" justify="center">
-            <Grid
-                templateColumns={["1fr", "1fr ", "1fr 1fr", "1fr 1fr 1fr"]}
-                mt="50px"
-                maxW={1800}
-                gap={6}
-            >
-                {imgURLs.map((img, index) => {
-                    return (
-                        <Flex
-                            key={index}
-                            minW="245px"
-                            maxH="400px"
-                            _hover={{
-                                backgroundColor: "black",
-                                borderRadius: "xl",
-                            }}
-                        >
-                            <Image
-                                src={`${img}`}
-                                w="100%"
-                                fit="cover"
-                                borderRadius="xl"
-                                _hover={{
-                                    opacity: "0.4",
-                                }}
-                                onClick={handleClick}
-                            />
-                        </Flex>
-                    );
-                })}
-                <ImgModal
-                    isOpen={isOpen}
-                    onClose={onClose}
-                    src={currentImgSrc}
-                ></ImgModal>
-            </Grid>
-        </Flex>
-    );
+function work() {
+    return <Gallery />;
 }
 
-export default Work;
+export default work;
